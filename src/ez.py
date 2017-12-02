507524e46e215a2ffb87f2ac7cac0747d662976b
mangefox <fox@fojji.se>
2017-12-02 02:34:17 +0100

    Updated to Python3

--- src/ez.py
+++ src/ez.py
@@ -29,7 +29,7 @@ Episode = namedtuple('Episode', 'name link age')
 
 def parse_args(args):
     if len(args) < 2:
-        print "Error: Missing arguments"
+        print("Error: Missing arguments")
 
     # if last argument is question mark, show list of eps
     if args[-1] == '?':
@@ -80,48 +80,37 @@ def filter_dupes(episodes):
 
 def get_episodes(searchterm):
     try:
-        # start_time = time.time()
-        # url = 'https://eztv.it/search/'
-        url = 'https://eztv.ag/search/'
-        # url = 'https://eztv-proxy.net/search/'
-        # url = 'http://eztv.bitproxy.eu/'
-        payload = {'SearchString': '',
-                   'SearchString1': searchterm,
-                   'search': 'Search'}
-        req = requests.post(url, data=payload, timeout=60, verify=False)
+        url = 'https://eztv.ag/search/%s'
+        req = requests.post(url % searchterm, timeout=60, verify=False)
         soup = BeautifulSoup(req.content, 'html.parser')
 
         hits = soup.findAll('a', attrs={'class': 'epinfo'})
         magnets = soup.findAll('a', attrs={'class': 'magnet'})
         released = soup.findAll('tr', attrs={'class': 'forum_header_border', 'name': 'hover'})
-
+        
         # zip into a list of tuples, one tuple for each episode (name, magnet link, age)
-        zipped = zip([h.text for h in hits],
+        zipped = list(zip([h.text for h in hits],
                      [m['href'] for m in magnets],
-                     [list(r.children)[9].text for r in released if len(list(r.children)) > 9])
+                     [list(r.children)[9].text for r in released if len(list(r.children)) > 9]))
 
         episodes = [Episode(z[0], z[1], z[2]) for z in zipped]
-        # print "GET took %.2f sec" % (time.time()-start_time)
         return episodes
     except:
-        print traceback.format_exc()
-        # print sys.exc_info()[0]
-        raw_input()
-        # print "Connection failed"
+        print(traceback.format_exc())
         exit()
 
 # --------------------------------------------------------------------------- #
 
 def show_list_selection(episodes):
-    print "Select episode to download:"
+    print("Select episode to download:")
     list_length = 15
     max_width = max(len(ep.name) for ep in episodes[:list_length])
     for index, ep in enumerate(episodes[:list_length]):
-        print "%2d. %s %s" % (index+1,
+        print("%2d. %s %s" % (index+1,
                               ep.name[:max_width].ljust(max_width+2),
-                              ep.age.ljust(9))
+                              ep.age.ljust(9)))
     try:
-        inp = raw_input()
+        inp = input()
     except KeyboardInterrupt:
         exit()
     if not str.isdigit(inp):
@@ -153,14 +142,14 @@ def main():
         episodes = filter_dupes(episodes)
 
     if len(episodes) == 0 or not episodes:
-        print "No hits."
+        print("No hits.")
         return
 
     if epnumber is None:
         epnumber = show_list_selection(episodes)
 
     if epnumber is not None:
-        print "Opening magnetic link for %s" % episodes[epnumber].name
+        print("Opening magnetic link for %s" % episodes[epnumber].name)
         webbrowser.open_new_tab(episodes[epnumber].link)
 
 # --------------------------------------------------------------------------- #
